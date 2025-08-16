73b46c9b172a4b3b963474e13d1fc090245b8e79
Pokkirispark1 <125215338+Pokkirispark1@users.noreply.github.com>
2025-08-16 21:57:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-API_ID = 12345678  # Get from https://my.telegram.org
-API_HASH = "your_api_hash_here"  # Get from https://my.telegram.org
-BOT_TOKEN = "your_bot_token_here"  # Get from @BotFather
+API_ID = 26592588  # Get from https://my.telegram.org
+API_HASH = "4f78c40e672ad86e10384cc8a0b43dc7"  # Get from https://my.telegram.org
+BOT_TOKEN = "8090595880:AAH5w6sgXU38DxoZ19T_Hfm-x7cZWcOapG4"  # Get from @BotFather
 
 # MongoDB Configuration
-MONGODB_URI = "mongodb://localhost:27017"  # Change to your MongoDB URI
-DATABASE_NAME = "telegram_bot"
+MONGODB_URI = "mongodb+srv://reactionbkots:reactionbkots@cluster0.onptwey.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Change to your MongoDB URI
+DATABASE_NAME = "Cluster0"
 
 # Admin Configuration
-ADMINS = [123456789, 987654321]  # Add admin user IDs here
+ADMINS = [1769132732, 560951157]  # Add admin user IDs here
 
 # Reaction Emojis
 R_EMOJIS = ["🤝", "😇", "🤗", "😍", "👍", "🎅", "😐", "🥰", "🤩", "😱", "🤣", "😘", "👏"]
